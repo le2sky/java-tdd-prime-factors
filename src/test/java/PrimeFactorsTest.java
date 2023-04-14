97802fdbbaf77b4ee19e0eacb7db20a9d53a7d87
leesky <leehaneul0623@gmail.com>
2023-04-14 23:33:57 +0900

    test : test for test

---
+++ src/test/java/PrimeFactorsTest.java
@@ -0,0 +1,12 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class PrimeFactorsTest {
+
+
+    @Test
+    void testForTest() {
+        assertThat(1).isEqualTo(1);
+    }
+}
