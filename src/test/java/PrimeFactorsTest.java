66f54fa24067f1c41f764594b9dbe05af5515014
leesky <leehaneul0623@gmail.com>
2023-04-15 22:35:35 +0900

    test : canfactorIntoPrimes for 2

--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -9,9 +9,13 @@ class PrimeFactorsTest {
     @Test
     void prime_factors() {
         assertThat(factors(1)).isEqualTo(Arrays.asList());
+        assertThat(factors(2)).isEqualTo(Arrays.asList(2));
     }
 
     private List<Integer> factors(int n) {
+        if (n == 2) {
+            return Arrays.asList(2);
+        }
         return Arrays.asList();
     }
 }
