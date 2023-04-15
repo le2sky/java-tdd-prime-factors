72d5e24421525c31dd76fd1d8592ad7f3306fb4f
leesky <leehaneul0623@gmail.com>
2023-04-15 22:34:16 +0900

    test : canfactorIntoPrimes for 1

--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -1,12 +1,17 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class PrimeFactorsTest {
 
-
     @Test
-    void testForTest() {
-        assertThat(1).isEqualTo(1);
+    void prime_factors() {
+        assertThat(factors(1)).isEqualTo(Arrays.asList());
+    }
+
+    private List<Integer> factors(int n) {
+        return Arrays.asList();
     }
 }
