12e35f7e7af503060c43b86ec5b82031672c8488
leesky <leehaneul0623@gmail.com>
2023-04-15 23:14:29 +0900

    refactor : change while into for

--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -26,14 +26,13 @@ class PrimeFactorsTest {
 
     private List<Integer> factors(int n) {
         List<Integer> factors = new ArrayList<>();
-        int divisor = 2;
-        while (n > 1) {
-            while (n % divisor == 0) {
-                n /= divisor;
+
+        for (int divisor = 2; n > 1; divisor++) {
+            for (; n % divisor == 0; n /= divisor) {
                 factors.add(divisor);
             }
-            divisor++;
         }
+        
         return factors;
     }
 }
