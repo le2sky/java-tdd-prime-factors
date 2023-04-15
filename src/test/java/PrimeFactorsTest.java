988965b25c7ba3e0dd9f81a1c322116f0cd2bd6a
leesky <leehaneul0623@gmail.com>
2023-04-15 22:54:09 +0900

    test : canFactorIntoPrimes for 4

--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -1,5 +1,6 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ class PrimeFactorsTest {
         assertFactors(1, Arrays.asList());
         assertFactors(2, Arrays.asList(2));
         assertFactors(3, Arrays.asList(3));
+        assertFactors(4, Arrays.asList(2, 2));
     }
 
     private void assertFactors(int n, List<Integer> list) {
@@ -18,9 +20,16 @@ class PrimeFactorsTest {
     }
 
     private List<Integer> factors(int n) {
+        List<Integer> factors = new ArrayList<>();
         if (n > 1) {
-            return Arrays.asList(n);
+            if (n % 2 == 0) {
+                n /= 2;
+                factors.add(2);
+            }
+            if (n > 1) {
+                factors.add(n);
+            }
         }
-        return Arrays.asList();
+        return factors;
     }
 }
